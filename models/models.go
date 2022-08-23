4af96cdf7aaea21bf4e41168f2cb6f6efaaa3d90
ditskihh <praditya@aiforesee.com>
2022-08-23 16:44:16 +0700

    Adding Function to query from database

--- models/models.go
+++ models/models.go
@@ -1,14 +1,27 @@
 package models
 
 import (
+	"awesomeProject1/config"
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 )
 
 type Response struct {
+	CompanyName       string  `json:"company_name"`
+	Npwp              string  `json:"npwp"`
+	BankDebtToEquity  float64 `json:"bank_debt_to_equity"`
+	Capitalisation    float64 `json:"capitalisation"`
+	GrossProfitMargin float64 `json:"gross_profit_margin"`
+	CurrentRatio      float64 `json:"current_ratio"`
+}
+
+type Finrats struct {
+	Id                string  `json:"id"`
 	CompanyName       string  `json:"company_name"`
 	Npwp              string  `json:"npwp"`
 	BankDebtToEquity  float64 `json:"bank_debt_to_equity"`
@@ -44,4 +57,35 @@ func GetCompany(Payload Payload) Response {
 	//fmt.Println(result.CompanyName)
 
 	return result
+}
+
+func FetchFinratio(CompanyNames string, Npwp string) Finrats {
+	db := config.CreateConnection()
+
+	var finrat Finrats
+
+	sqlStatement := fmt.Sprintf(`SELECT bld_loan_number, bpd_company_name, npwp, 
+coalesce(capitalisation,0) capitalisation,
+coalesce(gross_profit_margins,0) gross_profit_margins,
+coalesce(bank_debt_to_equity,0) bank_debt_to_equity,
+coalesce(current_ratio,0) current_ratio
+FROM data_pbk.fin_ratio_investree WHERE bpd_company_name='%s' and npwp='%s' limit 1`, CompanyNames, Npwp)
+
+	err := db.QueryRow(sqlStatement).Scan(&finrat.Id, &finrat.CompanyName,
+		&finrat.Npwp, &finrat.Capitalisation, &finrat.GrossProfitMargin, &finrat.BankDebtToEquity, &finrat.CurrentRatio)
+
+	_ = db.Close()
+
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No Data..")
+		finrat.CompanyName = "No Data"
+		return finrat
+	case nil:
+		return finrat
+	default:
+		log.Fatalf("Can't run query. %v", err)
+	}
+
+	return finrat
 }
