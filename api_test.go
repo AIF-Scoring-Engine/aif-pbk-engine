54d2772a2e3baf4e179a2f94e0077393e8a615de
ditskihh <praditya@aiforesee.com>
2022-08-25 09:57:27 +0700

    Rewrite test cases

--- api_test.go
+++ api_test.go
@@ -12,34 +12,51 @@ import (
 func TestPostOK(t *testing.T) {
 
 	var jsonTest = []byte(`{
-    "company_name": "ditskih.co",
-    "npwp":"1111",
-    "n_good_after_bad":72,
-    "outstanding_amount":1000000000,
-    "bank_umum":12,
-    "mob_newest_fac":123,
-    "lknb":12,
-    "n_act_cntr_in_1_m":123,
-    "no_of_closed_contracts":123}`)
+    "company_name": "merpati wahana raya",
+    "npwp":"21831094022000",
+    "api_key":"tj3hnFdcc31U"
+}`)
 
-	req, err := http.NewRequest("POST", "localhost:8080/api/post/company", bytes.NewBuffer(jsonTest))
+	req, err := http.NewRequest("POST", "localhost:8080/api/post/dev/company", bytes.NewBuffer(jsonTest))
 	if err != nil {
 		t.Fatal(err)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(controller.PostCompany)
+	handler := http.HandlerFunc(controller.PostCompanyDev)
 	handler.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-	expected := `{"company_name":"ditskih.co",` +
-		`"npwp":"1111",` +
-		`"bank_debt_to_equity":7.2,` +
-		`"capitalisation":3.32,` +
-		`"gross_profit_margin":1.23,` +
-		`"current_ratio":0.65}`
+	expected := `{"company_name":"merpati wahana raya","npwp":"21831094022000","bank_debt_to_equity":1.24137389744408,"capitalisation":3.18445164329251,"gross_profit_margin":0.445269105232753,"current_ratio":1.61141012964412}`
+	if strings.TrimRight(rr.Body.String(), "\n") != strings.TrimRight(expected, "\n") {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestPostWrongApiKey(t *testing.T) {
+
+	var jsonTest = []byte(`{
+    "company_name": "merpati wahana raya",
+    "npwp":"21831094022000",
+    "api_key":"tj3hnFcc31U"
+}`)
+
+	req, err := http.NewRequest("POST", "localhost:8080/api/post/dev/company", bytes.NewBuffer(jsonTest))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(controller.PostCompanyDev)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := `{"validationError":{"api_key":["api_key is wrong"]}}`
 
 	if strings.TrimRight(rr.Body.String(), "\n") != strings.TrimRight(expected, "\n") {
 		t.Errorf("handler returned unexpected body: got %v want %v",
@@ -47,32 +64,55 @@ func TestPostOK(t *testing.T) {
 	}
 }
 
-func TestPostNoData(t *testing.T) {
+func TestPostNoNpwp(t *testing.T) {
 
 	var jsonTest = []byte(`{
-    "company_name": "ditskiih.co",
-    "npwp":"1111",
-    "n_good_after_bad":72,
-    "outstanding_amount":1000000000,
-    "bank_umum":12,
-    "mob_newest_fac":123,
-    "lknb":12,
-    "n_act_cntr_in_1_m":123,
-    "no_of_closed_contracts":123}`)
+    "company_name": "merpati wahana raya",
+    "npwp":"",
+    "api_key":"tj3hnFdcc31U"
+}`)
 
-	req, err := http.NewRequest("POST", "localhost:8080/api/post/company", bytes.NewBuffer(jsonTest))
+	req, err := http.NewRequest("POST", "localhost:8080/api/post/dev/company", bytes.NewBuffer(jsonTest))
 	if err != nil {
 		t.Fatal(err)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(controller.PostCompany)
+	handler := http.HandlerFunc(controller.PostCompanyDev)
 	handler.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusBadRequest {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-	expected := `{"validationError":"No Data"}`
+	expected := `{"validationError":{"npwp":["Key is required"]}}`
+
+	if strings.TrimRight(rr.Body.String(), "\n") != strings.TrimRight(expected, "\n") {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestPostNoCompanyName(t *testing.T) {
+
+	var jsonTest = []byte(`{
+    "company_name": "",
+    "npwp":"21831094022000",
+    "api_key":"tj3hnFdcc31U"
+}`)
+
+	req, err := http.NewRequest("POST", "localhost:8080/api/post/dev/company", bytes.NewBuffer(jsonTest))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(controller.PostCompanyDev)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := `{"validationError":{"company_name":["Key is required"]}}`
 
 	if strings.TrimRight(rr.Body.String(), "\n") != strings.TrimRight(expected, "\n") {
 		t.Errorf("handler returned unexpected body: got %v want %v",
